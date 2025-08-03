c618e158bfcd16240bf7499114b85d2022ba18e3
PyroSoul56 <tomi.que.sa@gmail.com>
2025-08-03 07:20:36 -0500

    Inicializado el proyecto

---
+++ src/main/java/com/example/TrabajoSpringBoot/TrabajoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrabajoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoSpringBootApplication.class, args);
+	}
+
+}
